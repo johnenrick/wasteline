19ede54190473a64a8641a09dfbf78fe5a24f611
JOHN_ENRICK\johnenrick <plenosjohn@yahoo.com>
2016-01-09 22:24:09 +0800

    table component

--- application/modules/report_management/views/report_management_script.php
+++ application/modules/report_management/views/report_management_script.php
@@ -4,7 +4,6 @@
     reportManagement.initializeReportManagementTable = function(){
         var reportManagementTable = new TableComponent("#reportTableContainer");
         
-        
     };
     $(document).ready(function(){
         load_page_component("table_component", reportManagement.initializeReportManagementTable);
--- application/modules/system_application/views/page_component/table_component_script.php
+++ application/modules/system_application/views/page_component/table_component_script.php
@@ -1,7 +1,6 @@
 <script>
     var TableComponent = function(containerSelector){
         this.tableContainer = $(containerSelector);
-        console.log(this.tableContainer); 
         this.tableContainer.append($("#pageComponentContainer .table_component").clone());
     };
 </script>
