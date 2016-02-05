035e67efb03c8048cd1501ac37cbbb553fe8b594
patricksage <maningo.patricksage@gmail.com>
2016-02-05 23:17:11 +0800

    updated script
    can submit waste post
    w/o api

--- application/modules/system_application/views/waste_post_script.php
+++ application/modules/system_application/views/waste_post_script.php
@@ -2,10 +2,27 @@
 	var wastePostContainer = {};
 
 	$(document).ready(function(){
-
+		$("#wl-btn-side-submit").click(function(){
+			wastePostContainer.createWastePost();
+		});
+		$("#wl-btn-side-repost").click(function(){
+			alert("nope!");
+		});
 	});
 
 	wastePostContainer.createWastePost = function(){
-		
+		var waste_post_input = [];
+		$(".wl-rectangle-list").each(function(){
+			var container = {
+				description	: $(this).find(".wl-list-desciption").text(),
+				quantity	: $(this).find(".wl-list-quantity").text(),
+				price		: $(this).find(".wl-list-price").text(),
+				category	: $(this).find(".wl-list-category").text(),
+			}
+			
+			waste_post_input.push(container);
+		});
+
+		console.log(waste_post_input);
 	}
 </script>
