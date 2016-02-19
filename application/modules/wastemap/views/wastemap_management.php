390508b3131cd0dc77a4f6df15691b3a385433d3
khef22 <error22.mekhef@gmail.com>
2016-02-19 22:29:51 +0800

    update

--- application/modules/wastemap/views/wastemap_management.php
+++ application/modules/wastemap/views/wastemap_management.php
@@ -3,4 +3,30 @@
     <div id="wastemapContainer">
 
     </div>
+    <!-- footer container-->
+    <div id="wl-footer-content" class="wl-footer-map">
+        <div class="row form-inline">
+            <div class="col-md-9">
+            <div class="form-group">
+                <a class="btn btn-default capitalize wl-map-filter" data-toggle="tooltip" data-placement="top" title="" data-original-title="Click to display only Garbages"><span class="circle"></span>Garbage</a>
+                <a class="btn btn-default capitalize wl-map-filter" data-toggle="tooltip" data-placement="top" title="" data-original-title="Click to display only Services"><span class="circle"></span>Services</a>
+                <a class="btn btn-default capitalize wl-map-filter" data-toggle="tooltip" data-placement="top" title="" data-original-title="Click to display only Reports"><span class="circle"></span>Garbage Report</a>
+                <a class="btn btn-default capitalize wl-map-filter" data-toggle="tooltip" data-placement="top" title="" data-original-title="Click to display only Dumping Locations"><span class="circle"></span>Dumping Area</a>
+                <a class="btn btn-default capitalize wl-map-filter" data-toggle="tooltip" data-placement="top" title="" data-original-title="Click to display only Bryg. Hall Location"><span class="circle"></span>LGU Location</a>
+            </div>
+            </div>
+            <div class="col-md-3">
+                <div class="form-group">
+                    <div class="input-group">
+                        <input type="email" class="form-control wl-map-search-inpt" placeholder="Find something...">
+                        <span class="input-group-btn input-group-sm wl-map-search-btn">
+                            <button type="button" class="btn btn-fab btn-fab-mini wl-btn-map-search">
+                                <span class="lnr lnr-magnifier"></span>
+                            </button>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
--- application/modules/wastemap/views/wastemap_management_script.php
+++ application/modules/wastemap/views/wastemap_management_script.php
@@ -1,12 +1,18 @@
 <script>
     var wastemapManagement = {};
-    
+
     wastemapManagement.initializeWastemapManagement = function(){
         var wastemapManagement = new WebMapComponent("#wastemapContainer");
     };
     $(document).ready(function(){
         load_page_component("web_map_component", wastemapManagement.initializeWastemapManagement);
+
+        $('[data-toggle="tooltip"]').tooltip({
+            container: 'body'
+        });
+
+        $.material.ripples(".wl-map-filter, .wl-btn-map-search");
     });
 
-    
+
 </script>
