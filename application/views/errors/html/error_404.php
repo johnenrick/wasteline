30991eccd99a91c383a00b55c4ec670835527077
khef22 <error22.mekhef@gmail.com>
2016-02-24 03:49:47 +0800

    update 404 page

--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -7,58 +7,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <title>404 Page Not Found</title>
 <style type="text/css">
 
-::selection { background-color: #E13300; color: white; }
-::-moz-selection { background-color: #E13300; color: white; }
+::selection { background-color: #51984F; color: white; }
+::-moz-selection { background-color: #51984F; color: white; }
 
 body {
-	background-color: #fff;
-	margin: 40px;
-	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
+    background-image: url('<?=base_url();?>assets/images/404-error.png');
+    background-repeat: no-repeat;
+    background-color: #51984F;
+    background-position: center 40%;
 }
 
-a {
-	color: #003399;
-	background-color: transparent;
-	font-weight: normal;
-}
 
-h1 {
-	color: #444;
-	background-color: transparent;
-	border-bottom: 1px solid #D0D0D0;
-	font-size: 19px;
-	font-weight: normal;
-	margin: 0 0 14px 0;
-	padding: 14px 15px 10px 15px;
-}
-
-code {
-	font-family: Consolas, Monaco, Courier New, Courier, monospace;
-	font-size: 12px;
-	background-color: #f9f9f9;
-	border: 1px solid #D0D0D0;
-	color: #002166;
-	display: block;
-	margin: 14px 0 14px 0;
-	padding: 12px 10px 12px 10px;
-}
-
-#container {
-	margin: 10px;
-	border: 1px solid #D0D0D0;
-	box-shadow: 0 0 8px #D0D0D0;
-}
-
-p {
-	margin: 12px 15px 12px 15px;
-}
 </style>
 </head>
 <body>
-	<div id="container">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
-	</div>
+        <div style="font-family: Gotham;margin: 0 auto;width: 500px;text-align: center;margin-top: 30%;color: white;font-size: 22px;">
+                <p>The page you were looing for doesn't exist.</p>
+                <p>Return to <a href="<?=base_url();?>" style="color:#ff5722;text-decoration:none">homepage</a></p>
+            </div>
 </body>
 </html>
